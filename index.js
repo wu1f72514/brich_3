84f18bad104ab6a4c242374f392021e0cca2da77
wu1f72514 <wu1f72514@gmail.com>
2021-12-02 11:28:30 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-import store from './store'
-import './style.css'
-import './components/todoApp'
-import './components/userInfo'
+import store from './store';
+import './style.css';
+import './components/todoApp';
+import './components/userInfo';
 
 const App = new Vue({
   store: store,
-  el: '#app'
-})
+  el: '#app',
+  methods: {
+    injectHeadStylesheet(href) {
+      let link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = href;
+      document.append(link);
+    },
+  },
+  mounted: function () {
+    this.$nextTick(function () {
+      this.injectHeadStylesheet(
+        'https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css'
+      );
+    });
+  },
+});
